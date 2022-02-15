f0568ad7421343ace9e44686306662f4ad7a693e
Bappaditya Dutta <bappadityadutta2019@gmail.com>
2022-02-15 07:38:13 +0530

    O(1) Constant Time

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const everyone= ['dory','bruce','marlin','nemo','gill',
 
 const large= new Array(100000).fill('nemo');
 
+
+//As the Elements increased number of operations increased, which represents O(n)
+// Straight lines
 function findNemo(array){
 
     for(let i=0;i<array.length; i++){
@@ -17,5 +20,14 @@ function findNemo(array){
 
 findNemo(large);
 
-//As the Elements increased number of operations increased, which represents O(n)
-// Straight lines
+// Demonstrating O(1) Constant Time
+
+const boxes=[1,2,3,4,5];
+
+function logFirstTwoItems(array){
+    console.log(array[0]);//O(1)
+    console.log(array[1]);//O(2)
+}
+
+logFirstTwoItems(boxes);
+
